c2c2ab0f00804bcd4d8f4515ac377c7c2a798837
InternalErrorGit <phil.gatzka@gmail.com>
2021-12-08 13:07:14 +0100

    Added Share screen

---
+++ app/src/main/java/ch/zli/pg/app/view/ShareView.java
@@ -0,0 +1,16 @@
+package ch.zli.pg.app.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.zli.pg.contactscan.R;
+
+public class ShareView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_view);
+    }
+}

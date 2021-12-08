777053312f59741d623ad80c034d7c051c824487
InternalErrorGit <phil.gatzka@gmail.com>
2021-12-08 13:02:18 +0100

    Added Menu and Splash screen

---
+++ app/src/main/java/ch/zli/pg/app/view/MenuView.java
@@ -0,0 +1,16 @@
+package ch.zli.pg.app.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.zli.pg.contactscan.R;
+
+public class MenuView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_view);
+    }
+}
---
+++ app/src/main/java/ch/zli/pg/app/view/SplashView.java
@@ -0,0 +1,16 @@
+package ch.zli.pg.app.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ch.zli.pg.contactscan.R;
+
+public class SplashView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_view);
+    }
+}
